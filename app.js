169e77a90553110c4a0d16e66049d1759217e4a5
ASanzL <andreas.sanz@hotmail.com>
2023-05-10 14:45:54 +0200

    ._.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const connection = mysql.createConnection({
   host: process.env['MYSQL_HOST'],
   user: process.env['MYSQL_USER'],
   password: process.env['MYSQL_PASSWORD'],
-  database: 'db'
+  database: ['MYSQL_DATABASE']
 });
 console.log(process.env['MYSQL_HOST'],process.env['MYSQL_USER'],process.env['MYSQL_PASSWORD'],process.env['MYSQL_DATABASE']);
 connection.connect();
