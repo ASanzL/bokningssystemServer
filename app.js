09e213c9a3afd3a4c14bae0e232914530dbb2a0e
ASanzL <andreas.sanz@hotmail.com>
2023-05-11 16:34:53 +0200

    Little update

--- app.js
+++ app.js
@@ -1,48 +1,62 @@
 const dotenv = require('dotenv').config();
-
 var express = require('express');
+const bodyParser = require('body-parser');
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-app.route('/api/test')
-.get((req ,res) => {
-  let x = ':-)';
-  const mysql = require('mysql')
+const mysql = require('mysql');
+const { DATETIME } = require('mysql/lib/protocol/constants/types');
 
-  var connection=mysql.createConnection({
-    host: process.env['MYSQL_HOST'],
-    user: process.env['MYSQL_USER'],
-    password: process.env['MYSQL_PASSWORD'],
-    database: process.env['MYSQL_DATABASE'],
-    port: process.env['MYSQL_PORT'],
-  });
-  console.log(process.env['MYSQL_HOST'],process.env['MYSQL_USER'],process.env['MYSQL_PASSWORD'],process.env['MYSQL_DATABASE']);
-  connection.connect();
+var connection=mysql.createConnection({
+  host: process.env['MYSQL_HOST'],
+  user: process.env['MYSQL_USER'],
+  password: process.env['MYSQL_PASSWORD'],
+  database: process.env['MYSQL_DATABASE'],
+  port: process.env['MYSQL_PORT'],
+});
+connection.connect();
+
+
+app.route('/api/test')
+.get((req, res) => {
   connection.query('SELECT * FROM users', (err, rows, fields) => {
     if (err) throw err;
-
-    x = rows[0];
-    res.send({msg: 'Secret: ' + x['name'] + ' from API'});
+    for (const r of rows) {
+      console.log(r.name  );
+    }
+    res.send({msg: 'Secret: ' + rows[0].name + ' from API'});
   });
-  connection.end();
 });
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.route('/api/airplanes')
+.get((req, res) => {
+  connection.query('SELECT * FROM airplanes', (err, rows, fields) => {
+    if (err) throw err;
+    res.send({airplanes: rows});
+  });
+});
 
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+app.route('/api/book')
+.post((req, res) => {
+  console.log(req.body);
+  let startTime = new Date(req.body.startTime);
+  startTime = startTime.toISOString().slice(0, 19).replace('T', ' ');
+  let endTime = new Date(req.body.endTime);
+  endTime = endTime.toISOString().slice(0, 19).replace('T', ' ');
+  // startTime = `${startTime.getFullYear()}${startTime.getUTCMonth()}${startTime.getDate()}${startTime.getHours()}${startTime.getMinutes()}${startTime.getSeconds()}`;
+  console.log(startTime);
+  // insert into booking (personnummer, airplanesId, startTime, endTime) values (12341122-1234, 1, NOW(), NOW());
+  connection.query(
+    `insert into booking (personnummer, airplanesId, startTime, endTime) values (${req.body.personnummer}, ${req.body.airplanesId}, '${(startTime)}', '${(endTime)}')`,
+    // `INSERT INTO booking (personnummer, airplanesId, startTime, endTime) VALUES ("${req.body.personnummer}" "${req.body.airplanesId}" "${req.body.startTime}" "${req.body.endTime}" )`,
+    (err, rows, fields) => {
+      if (err) throw err;
+      res.send(200);
+  });
+});
 
 module.exports = app;
--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,10 +1,47 @@
-const t = document.getElementById('test');
+// const t = document.getElementById('test');
 
-async function testFunction(){
-    const response = await fetch("/api/test");
-    const jsonData = await response.json();
+// async function testFunction(){
+//     document.getElementById("knapp").addEventListener('click',(e)=> {console.log('hej');});
+//     const response = await fetch("/api/test");
+//     const jsonData = await response.json();
 
-    t.textContent = jsonData.msg;
+//     t.textContent = jsonData.msg;
+// }
+
+document.getElementById("knapp").addEventListener('click', onclick);
+function makeRequest(){
+    var xhr = new XMLHttpRequest();
+    var url = "/api/book";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    var data = JSON.stringify({ "personnummer": document.getElementById("uid").value, "airplanesId": document.getElementById("planes").value, "startTime": document.getElementById("starttime").value, "endTime": document.getElementById("endtime").value });
+    console.log(data)
+    xhr.send(data);
+
+
 }
 
-testFunction();
+async function handleResponse(){
+    var xhr = new XMLHttpRequest();
+    var url = "/api/airplanes";
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    console.log(jsonData);
+    select = document.getElementById("planes");
+    for(e of jsonData.airplanes){
+        console.log(e);
+        newer = document.createElement('option');
+        newer.appendChild(document.createTextNode(e.name));
+        newer.value = e.id;
+        select.appendChild(newer);
+    }
+}
+
+
+function onclick(){
+    makeRequest();
+    console.log('at least the button work');
+    
+}
+
+handleResponse();
